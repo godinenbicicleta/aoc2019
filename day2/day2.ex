bb2c6bdff4adc92d89d08019b55eca9010f7436d
Bruno Carballo <carballo.bruno@gmail.com>
2019-12-01 22:04:25 -0800

    add system.halt so program stops once solution is found

--- day2/day2.ex
+++ day2/day2.ex
@@ -1,40 +1,46 @@
 defmodule Day2 do
   def read do
-    nums = File.read!("input.txt")
-           |> String.trim
-           |> String.split(",")
-           |> Enum.map(&String.to_integer/1)
-           |> Enum.to_list
-    
+    nums =
+      File.read!("input.txt")
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.to_list()
+
     process({:ok, nums, 0})
-           |> IO.inspect
+    |> IO.inspect()
   end
+
   def test do
-    nums = [1,9,10,3,2,3,11,0,99,30,40,50]
+    nums = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+
     process({:ok, nums, 0})
-    |> IO.inspect
+    |> IO.inspect()
   end
+
   defp process({:ok, nums, index}) do
     [code, pos1, pos2, pos3] = Enum.slice(nums, index, 4)
+
     case code do
-      1 -> {:ok, sum( pos1, pos2, pos3, nums), index + 4}
-      2 -> {:ok, multiply(pos1, pos2, pos3,nums), index + 4}
+      1 -> {:ok, sum(pos1, pos2, pos3, nums), index + 4}
+      2 -> {:ok, multiply(pos1, pos2, pos3, nums), index + 4}
       99 -> {:stop, nums}
     end
     |> process
   end
+
   defp process({:stop, nums}) do
     nums
   end
 
   defp sum(pos1, pos2, pos3, nums) do
-    List.replace_at(nums, pos3, Enum.at(nums,pos1)+Enum.at(nums,pos2))
+    List.replace_at(nums, pos3, Enum.at(nums, pos1) + Enum.at(nums, pos2))
   end
 
   defp multiply(pos1, pos2, pos3, nums) do
-    List.replace_at(nums, pos3, Enum.at(nums,pos1)*Enum.at(nums,pos2))
+    List.replace_at(nums, pos3, Enum.at(nums, pos1) * Enum.at(nums, pos2))
   end
 end
 
-#Day2.test()
+# Day2.test()
 Day2.read()
--- day2/day2_p2.ex
+++ day2/day2_p2.ex
@@ -1,47 +1,51 @@
 defmodule Day2 do
   def read do
-    nums = File.read!("input.txt")
-           |> String.trim
-           |> String.split(",")
-           |> Enum.map(&String.to_integer/1)
-           |> Enum.to_list
+    nums =
+      File.read!("input.txt")
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.to_list()
 
-    for i1 <- 0..99, i2 <- 0..99  do
+    for i1 <- 0..99, i2 <- 0..99 do
       case attempt(nums, i1, i2) do
-        [19690720|_] -> IO.inspect(100*i1+i2)
+        [19_690_720 | _] -> 
+          IO.inspect(100 * i1 + i2)
+          System.halt(0)
         _ -> nil
       end
-
     end
-    
   end
 
-  defp attempt(nums, i1,i2) do
-    nums = List.replace_at(nums, 1,i1 )
-           |> List.replace_at( 2, i2)
+  defp attempt(nums, i1, i2) do
+    nums =
+      List.replace_at(nums, 1, i1)
+      |> List.replace_at(2, i2)
 
     process({:ok, nums, 0})
   end
-  
+
   defp process({:ok, nums, index}) do
     [code, pos1, pos2, pos3] = Enum.slice(nums, index, 4)
+
     case code do
-      1 -> {:ok, sum( pos1, pos2, pos3, nums), index + 4}
-      2 -> {:ok, multiply(pos1, pos2, pos3,nums), index + 4}
+      1 -> {:ok, sum(pos1, pos2, pos3, nums), index + 4}
+      2 -> {:ok, multiply(pos1, pos2, pos3, nums), index + 4}
       99 -> {:stop, nums}
     end
     |> process
   end
+
   defp process({:stop, nums}) do
     nums
   end
 
   defp sum(pos1, pos2, pos3, nums) do
-    List.replace_at(nums, pos3, Enum.at(nums,pos1)+Enum.at(nums,pos2))
+    List.replace_at(nums, pos3, Enum.at(nums, pos1) + Enum.at(nums, pos2))
   end
 
   defp multiply(pos1, pos2, pos3, nums) do
-    List.replace_at(nums, pos3, Enum.at(nums,pos1)*Enum.at(nums,pos2))
+    List.replace_at(nums, pos3, Enum.at(nums, pos1) * Enum.at(nums, pos2))
   end
 end
 
